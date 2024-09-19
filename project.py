1c8cfb91039e3daca80e9619fdd20fc9225b7def
sohan rokka <83445322+sunandaroka9@users.noreply.github.com>
2024-09-19 14:22:41 +0545

    i did more work on main

--- project.py
+++ project.py
@@ -30,12 +30,25 @@ def main:
         print(f"invalid master password")
         return
     else:
-        option = int(input("\noptions\n1)add new password\n2)retrive password\n3)generate rendom password\n4)delete password\n5)change password\n6)quit\nchoose an option(1-6)"))
-        if option == 1:
-            add_new_password()
-        elif option == 2:
-            retrive_password(site,username)
-
+        try:
+            option = int(input("\noptions\n1)add new password\n2)retrive password\n3)generate random password\n4)delete password\n5)change password\n6)quit\nchoose an option(1-6)"))
+            if option == 1:
+                add_new_password()
+            elif option == 2:
+                retrive_password(site,username)
+            elif option == 3:
+                generate_random_password()
+            elif option == 4:
+                delete_password()
+            elif option == 5:
+                change_password()
+            elif option == 6:
+                break
+            else:
+                print("invalid option")
+        except ValueError:
+            print("enter a fucking number next time u dimwit")
+            return
 
 if "__name__" == __main__ :
     main()
