fefbe41a9d40d3e678a3242ba23aa1b4d4ec6eb0
sohan rokka <83445322+sunandaroka9@users.noreply.github.com>
2024-09-19 14:08:36 +0545

    so i made main and laid up work for other functions

--- project.py
+++ project.py
@@ -4,11 +4,37 @@ import base64
 from getpass import getpass
 from cryptography.fernet import fernet
 
+def add_new_password():
+    ....
+    
+    
+def retrive_password(site,username):
+    ....
+    
+def generate_random_password():
+    ....
 
+
+def delete_password(site, username):
+    ....
+
+
+def change_password(site, username, new_password):
+    .....
 
 
 def main:
-    ...
+    master_pass = input("Welcome to the password manager /n enter your master password: ")
+    
+    if master_pass != __master__password:
+        print(f"invalid master password")
+        return
+    else:
+        option = int(input("\noptions\n1)add new password\n2)retrive password\n3)generate rendom password\n4)delete password\n5)change password\n6)quit\nchoose an option(1-6)"))
+        if option == 1:
+            add_new_password()
+        elif option == 2:
+            retrive_password(site,username)
 
 
 if "__name__" == __main__ :
